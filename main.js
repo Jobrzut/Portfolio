f24d6726e46a22baadb3d98e5e88eb1f2181a98b
Julek <90630830+Jobrzut@users.noreply.github.com>
2023-01-12 21:50:00 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,12 @@ const theme = () => { //zmienianie klasy
         themeIcon.style.background = '#fff' //jasny
     }
 }
+
+// dzialanie menu
+const menuToggle = document.querySelector('.toggle')
+const showcase = document.querySelector('.showcase') 
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+    showcase.classList.toggle('active')
+})
